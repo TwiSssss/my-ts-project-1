aeb7c0c8d17d19bc8765d4cb350fdcf93a6f45b5
Vlad Tsokota <v.tsokota.69@gmail.com>
2025-05-22 11:54:58 +0300

    last

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -4,18 +4,15 @@ type User = {
     email: string,
     password: string,
 };
-
 const user: User = {
     name: "Vlad",
     surname: "Tsok",
     email: "wwwww@gm.com",
     password: "qwerty",
 };
-
 function createOrUpdateUser(initialValues: Partial<User>): User {
     return { ...user, ...initialValues };
 }
-
 createOrUpdateUser({
     email: "user@mail.com",
     password: "password123",
